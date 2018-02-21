b4dced4352b7722e5cdd535148991e9e3fa87d2b
Ryan F. Lee <leery@ldschurch.org>
2018-02-20 23:05:46 -0700

    added PyCharm config files, renamed util module, added a few things to model.py

--- src/algorithms/sam/model.py
+++ src/algorithms/sam/model.py
@@ -1,14 +1,36 @@
-import algorithms.sam.math_util as math_util
+import src.algorithms.sam.util as util
 import numpy as np
 
 class SAM:
-    def __init__(self):
-        self.convergence = False
-        self.epsilon = 0.001
+    def __init__(self, corpus):
+
+        # when stopping criteria hasn't changed by more than
+        # epsilon for a few iterations, stop topic discovery
+        self.CONVERGENCE = False
+        self.EPSILON = 0.001
+        self.LAMBDA = 0.0001
+
+        self.analyze_data(corpus)
+
+        # initialize model hyperparameters
+        self.num_topics = 10
+
+        self.xi = 5.0
+        self.m = 10.0
+        self.k0 = 50.0
+        self.k = 500.0
+
+        # initialize variational parameters
+        self.vAlpha = np.rand(self.num_docs, self.num_topics)
+        self.vMu = np.rand(self.vocab_size, self.num_topics)
+        self.vM = np.sum(self.v_mu, axis=1)
 
     def analyze_data(self, corpus):
+        # TODO: actually read in and store vocab / documents
         self.vocabulary = []
         self.vocab_size = len(self.vocabulary)
+        self.documents = []
+        self.num_docs = len(self.documents)
 
     def update_model_params(self, xi, m, alpha, kappa_0, kappa):
         pass
@@ -18,15 +40,15 @@ class SAM:
 
     def do_EM(self):
         old_result = 0
-        while not self.convergence:
+        while not self.CONVERGENCE:
             self.do_E()
             new_result = self.do_M()
-            if new_result - old_result < self.epsilon:
-                self.convergence = True
+            if new_result - old_result < self.EPSILON:
+                self.CONVERGENCE = True
 
     def do_E(self):
-        A_V_xi = math_util.bessel_approx(self.vocab_size, self.xi)
-        A_V_k0 = math_util.bessel_approx(self.vocab_size, self.k0)
+        A_V_xi = util.bessel_approx(self.vocab_size, self.xi)
+        A_V_k0 = util.bessel_approx(self.vocab_size, self.k0)
 
-        topic_mean_sum = np.sum(self.vmu)
-        delta_vm = self.k0*A_V_k0*self.m + A_V_xi*A_V_k0*self.xi*topic_mean_sum + 2*LAMBA*self.vm
+        topic_mean_sum = np.sum(self.vMu)
+        delta_vM = self.k0*A_V_k0*self.m + A_V_xi*A_V_k0*self.xi*topic_mean_sum + 2*self.LAMBA*self.vM
--- src/algorithms/sam/util.py
+++ src/algorithms/sam/util.py
@@ -2,9 +2,13 @@ import math.log as log
 import math.sqrt as sqrt
 import math.pi as pi
 
+# corresponds to avk() in original SAM code
+# not sure why they do it differently, this is what's in
+# the paper they reference
 def bessel_approx(v, z):
     ratio_z_v = z / v
     alpha = 1 + (ratio_z_v*ratio_z_v)
     eta = sqrt(alpha) + log(ratio_z_v) - log(1 + sqrt(alpha))
 
     return -log(sqrt(2*pi*v)) + (v*eta) - (0.25*log(alpha))
+
