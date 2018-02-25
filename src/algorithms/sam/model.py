7225af53ff9889d3ecb7d0026f5dbc65108185b3
Ryan F. Lee <leery@ldschurch.org>
2018-02-24 17:14:39 -0700

    improvements to model, added l2_normalize method to util

--- src/algorithms/sam/model.py
+++ src/algorithms/sam/model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class SAM:
-    def __init__(self, corpus, stopwords=None):
+    def __init__(self, corpus, stopwords=None, topics=10):
 
         # when stopping criteria hasn't changed by more than
         # epsilon for a few iterations, stop topic discovery
@@ -21,23 +21,37 @@ class SAM:
         self.num_docs = self.documents.shape[0]
 
         # initialize model hyperparameters
-        self.num_topics = 10
+        self.num_topics = topics
 
-        self.xi = 5.0
-        self.m = 10.0
+        self.xi = 500.0
+        self.m = util.l2_normalize(np.random.rand(self.vocab_size))
+        self.alpha = np.random.rand(self.num_topics, self.num_docs)
         self.k0 = 50.0
         self.k = 500.0
 
         # initialize variational parameters
-        self.vAlpha = np.random.rand(self.num_docs, self.num_topics)
-        self.vMu = np.random.rand(self.vocab_size, self.num_topics)
-        self.vM = np.sum(self.vMu, axis=1)
+        self.vAlpha = np.random.rand(self.num_topics, self.num_docs)
+        self.vMu = util.l2_normalize(np.random.rand(self.vocab_size, self.num_topics))
+        self.vM = util.l2_normalize(np.random.rand(self.vocab_size))
 
-    def update_model_params(self, xi, m, alpha, kappa_0, kappa):
-        pass
+    def update_model_params(self):
 
-    def update_free_params(self, v_alpha, v_mu, v_m):
-        pass
+        # self.xi =
+        self.m = util.l2_normalize(np.sum(self.vMu, axis=1))
+        # self.alpha =
+
+    def update_free_params(self):
+        A_V_xi = util.bessel_approx(self.vocab_size, self.xi)
+        A_V_k0 = util.bessel_approx(self.vocab_size, self.k0)
+        topic_mean_sum = np.sum(self.vMu)
+
+        # self.vAlpha =
+
+        # self.vMu =
+
+        self.vM = util.l2_normalize(self.k0 * A_V_k0 * self.m +
+                                    A_V_xi * A_V_k0 * self.xi *
+                                    topic_mean_sum + 2 * self.LAMBDA * self.vM)
 
     def do_EM(self):
         old_result = 0
@@ -48,21 +62,18 @@ class SAM:
                 self.CONVERGENCE = True
 
     def do_E(self):
-        A_V_xi = util.bessel_approx(self.vocab_size, self.xi)
-        A_V_k0 = util.bessel_approx(self.vocab_size, self.k0)
-
-        topic_mean_sum = np.sum(self.vMu)
-        delta_vM = self.k0*A_V_k0*self.m + A_V_xi*A_V_k0*self.xi*topic_mean_sum + 2*self.LAMBDA*self.vM
-        delta_vAlpha = 0
-        delta_vMu = 0
+        self.update_free_params()
+        self.update_model_params()
 
     def do_M(self):
         return 0
 
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--corpus', required=True, help='Path to the location of your corpus. This can be either a directory or a single file.')
+    parser.add_argument('-c', '--corpus', required=True, help='Path to the location of your corpus. ' +
+                                                              'This can be either a directory or a single file.')
     parser.add_argument('-s', '--stopwords', help='Optional path to a file containing stopwords.')
     args = parser.parse_args()
 
--- src/algorithms/sam/util.py
+++ src/algorithms/sam/util.py
@@ -1,4 +1,6 @@
 from math import log,sqrt,pi
+import numpy as np
+
 
 # corresponds to avk() in original SAM code
 # not sure why they do it differently, this is what's in
@@ -9,4 +11,18 @@ def bessel_approx(v, z):
     eta = sqrt(alpha) + log(ratio_z_v) - log(1 + sqrt(alpha))
 
     return -log(sqrt(2*pi*v)) + (v*eta) - (0.25*log(alpha))
+
+
+def l2_normalize(data):
+    arr_data = np.asmatrix(data)
+    if len(arr_data.shape) == 1:
+        l2_norm = np.sqrt(np.sum(np.multiply(arr_data * arr_data)))
+        return data / l2_norm
+
+    elif len(arr_data.shape) == 2:
+        col_norms = np.sqrt(np.sum(np.multiply(arr_data, arr_data), axis=1))
+        return data / col_norms
+
+    else:
+        raise Exception('Data may only have 1 or 2 dimensions')
 
