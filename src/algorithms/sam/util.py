5b4a4a44407f48855c0b6c4d4c9ce185c1bca085
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2018-03-01 16:22:58 -0700

    updating math utils

--- src/algorithms/sam/util.py
+++ src/algorithms/sam/util.py
@@ -21,10 +21,27 @@ def bessel_approx(v, z):
     eta = sqrt(alpha) + log(ratio_z_v) - log(1 + sqrt(alpha))
 
     return -log(sqrt(2*pi*v)) + (v*eta) - (0.25*log(alpha))
-
+    #return -log(sqrt(2*pi*v)) + (v*(sqrt(1 + z / v*z / v) + log(z / v) - log(1 + sqrt(1 + z / v*z / v)))) - (0.25*log(1 + z / v*z / v))
+    
 def avk(v,z):
     return bessel_approx(v,z)
+"""    
+    assert np.isscalar(v)
+    assert np.isscalar(k)
+    return (np.sqrt((v / k) ** 2 + 4) - v / k) / 2.0
+"""
 
+def bessel_approx_derivative(v,k):
+    -log(sqrt(2*pi*v)) + (v*eta) - (0.25*log(alpha))
+    
+def avk_derivative(v, k):
+    #Derivative of the VMF mean resultant length wrt kappa.    
+    #a = AvK(v,k);
+    #deriv = 1-a^2 - (v-1)/k*a;
+    #return -1/2/(v^2/k^2+4)^(1/2)*v^2/k^3+1/2*v/k^2
+    return -0.5 / (v**2/k**2+4)**0.5 * v**2/k**3 + 0.5*v/k**2
+
+    
 def l2_normalize(data):
     arr_data = np.asmatrix(data)
     if len(arr_data.shape) == 1:
